3b4b6e15dbcd15ed52ed7ff7041c4a11f5f4711a
fiah4776 <107185010+fiah4776@users.noreply.github.com>
2025-01-22 12:06:06 +0100

    count button inplemented

---
+++ app/src/main/java/com/example/butooncount/testtestfiah4667/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.butooncount.testtestfiah4667
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    var count = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        val textArea = findViewById<TextView>(R.id.textView)
+
+        button.setOnClickListener{
+            count++
+            textArea.text = count.toString()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
